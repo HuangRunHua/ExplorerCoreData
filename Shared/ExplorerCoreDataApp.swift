364fdf16d09d1d2f7253687511000156be3998a0
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-03-13 10:52:12 +0800

    Initial Commit

---
+++ Shared/ExplorerCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerCoreDataApp.swift
+//  Shared
+//
+//  Created by Runhua Huang on 2022/3/13.
+//
+
+import SwiftUI
+
+@main
+struct ExplorerCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
